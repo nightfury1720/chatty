36ada9743f05e7415941e79d8a90c26b3322469d
Suraj <skd18@iitbbs.ac.in>
2023-11-21 19:48:34 +0530

    created register page jsx

---
+++ public/src/pages/Register.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+function Register() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("form");
+  };
+  const handleChange = (event) => {
+
+  };
+  return (
+    <>
+      <FormContainer>
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src="" alt="" />
+            <h1>Chatty</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account ? 
+            {/* <Link to="/login">Login.</Link> */}
+          </span>
+        </form>
+      </FormContainer>
+    </>
+  );
+}
+
+const FormContainer = styled.div``;
+
+export default Register;
---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("DB connection successfull", process.env.MONGO_URL);
+}).catch((err) => console.log(err.message));
+
+const server =app.listen(process.env.PORT, () => {
+    console.log(`Server Started on Port ${process.env.PORT}`);
+})
